32bc81723f333fdffba5fd19203ef18129faad59
Student <student@rj.local>
2016-12-10 11:01:48 -0500

    it now has the circle the square and the cross in the correct positions

--- ZennerGuesser/GameScene.swift
+++ ZennerGuesser/GameScene.swift
@@ -18,6 +18,21 @@ class GameScene: SKScene {
         
         // background is plain white
         backgroundColor = SKColor.white
+        
+        let cross = SKSpriteNode(imageNamed: "Cross") //thats the cross card
+        cross.position = CGPoint(x: size.width/2, y: size.height/2) // where the cross goes
+        cross.setScale(3.0)
+        addChild(cross) //this puts cross in
+        
+        let square = SKSpriteNode(imageNamed: "Square") //thats the Square card
+        square.position = CGPoint(x: size.width/2.75, y: size.height/4) // where the square goes
+        square.setScale(3.0)
+        addChild(square) //this puts square in
+        
+        let circle = SKSpriteNode(imageNamed: "Circle") //thats the Circle card
+        circle.position = CGPoint(x: size.width/2.75, y: size.height/4*3) // where the Circle goes
+        circle.setScale(3.0)
+        addChild(circle) //this puts Circle in
     }
 }
 
