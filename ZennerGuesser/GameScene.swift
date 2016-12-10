4d9e3268f2ddae077cb59e18ba971a271a5db1ee
Student <student@rj.local>
2016-12-10 13:35:40 -0500

    when ever its clicked a card displays its own number at the HUD very important

--- ZennerGuesser/GameScene.swift
+++ ZennerGuesser/GameScene.swift
@@ -14,6 +14,11 @@ class GameScene: SKScene {
     //basic score label
     let selectionLabel = SKLabelNode(fontNamed: "Helvetica-Bold")
     var selected = 0
+    let cross = SKSpriteNode(imageNamed: "Cross") //thats the cross card
+    let square = SKSpriteNode(imageNamed: "Square") //thats the Square card
+    let circle = SKSpriteNode(imageNamed: "Circle") //thats the Circle card
+    let star = SKSpriteNode(imageNamed: "Star") //thats the Star card
+    let wavylines = SKSpriteNode(imageNamed: "WavyLines") //thats the squiggle card
     
     override func didMove(to view: SKView) {
         
@@ -21,27 +26,25 @@ class GameScene: SKScene {
         // background is plain white
         backgroundColor = SKColor.white
         
-        let cross = SKSpriteNode(imageNamed: "Cross") //thats the cross card
         cross.position = CGPoint(x: size.width/2, y: size.height/2) // where the cross goes
         cross.setScale(3.0)
         addChild(cross) //this puts cross in
         
-        let square = SKSpriteNode(imageNamed: "Square") //thats the Square card
         square.position = CGPoint(x: size.width/2.75, y: size.height/4) // where the square goes
         square.setScale(3.0)
         addChild(square) //this puts square in
         
-        let circle = SKSpriteNode(imageNamed: "Circle") //thats the Circle card
+        
         circle.position = CGPoint(x: size.width/2.75, y: size.height/4*3) // where the Circle goes
         circle.setScale(3.0)
         addChild(circle) //this puts Circle in
         
-        let star = SKSpriteNode(imageNamed: "Star") //thats the Star card
+        
         star.position = CGPoint(x: size.width/1.60, y: size.height/4*3) // where the star goes
         star.setScale(3.0)
         addChild(star) //this puts star in
         
-        let wavylines = SKSpriteNode(imageNamed: "WavyLines") //thats the squiggle card
+        
         wavylines.position = CGPoint(x: size.width/1.60, y: size.height/4) // where the squiggles goes
         wavylines.setScale(3.0)
         addChild(wavylines) //this puts star in
@@ -54,8 +57,49 @@ class GameScene: SKScene {
         selectionLabel.position = CGPoint(x: size.width/2, y: size.height/15)
         addChild(selectionLabel)
 
+        //this lets scene respond too touch
     }
+    
+    //this lets scene respond too touch
+    override func touchesBegan(_ touches: Set<UITouch>,
+                               with event:UIEvent?) {
+        
+        // a touch has many points because fingers are big
+        guard let touch = touches.first else {
+            return
+        }
+        
+        //get the location of the first touch
+        let touchLocation = touch.location(in:self)
+        
+        //call a function to cause the cards reaction
+        if circle.frame.contains(touchLocation) {
+            selected = 1
+            selectionLabel.text = String(selected)
+            print("clicked circle")
+        } else if star.frame.contains(touchLocation){
+            selected = 2
+            selectionLabel.text = String(selected)
+            print ("clicked star")
+            
+        }else if square.frame.contains(touchLocation){
+            selected = 3
+            selectionLabel.text = String(selected)
+            print ("clicked square")
+        }else if wavylines.frame.contains(touchLocation){
+            selected = 4
+            selectionLabel.text = String(selected)
+        
+            print ("clicked wavy lines")
+        }else if cross.frame.contains(touchLocation){
+            selected = 5
+            selectionLabel.text = String(selected)
+            print ("clicked cross")
+        
+    }
+
+    
 }
 
 
-
+}
